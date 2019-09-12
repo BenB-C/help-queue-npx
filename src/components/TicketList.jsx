554ca4f618327edb0a25bc2229339e3b520c4c0e
benb-c <bcauchon@gmail.com>
2019-09-11 17:12:12 -0700

    Add conditional functionality depending on route

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -13,6 +13,7 @@ function TicketList(props){
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
+          currentRouterPath={props.currentRouterPath}
           key={ticket.id}/>
       )}
     </div>
@@ -20,7 +21,8 @@ function TicketList(props){
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array
+  ticketList: PropTypes.array,
+  currentRouterPath: PropTypes.string
 };
 
 export default TicketList;
